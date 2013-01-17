4724cb28c18d51de7150c7d7726c85526acf2520
zhang bo <zhangbo13579@gmail.com>
2013-01-17 16:25:01 +0800

    文章日期

--- generator.rb
+++ generator.rb
@@ -7,19 +7,28 @@ require "maruku"
 @posts = Array.new
 Dir.foreach("_posts") do |x|
   unless x == "." or x == ".."
-    title = x
+    title = x.match('(?<=.{11})(.*)(?=\..*)')[0]
+    created_date = x[0,10]
     content = File.new("_posts/#{x}").read
     doc = Maruku.new(content)
     content = doc.to_html
-    post = Post.new(title, content)
+
+    post = Post.new(title, content, created_date)
     @posts << post
   end
 end
 
-f = File.new("layout.erb")
+f = File.new("_layouts/index.html")
 html = ERB.new(f.read).result(binding)
-puts html
 
 File.open("index.html", "w") do |x|
   x.puts(html)
+end
+
+@posts.each do |post|
+  template = File.new("_layouts/show.html")
+  html = ERB.new(template.read).result(binding)
+  File.open("_sites/#{post.url}", "w") do |x|
+    x.puts(html)
+  end
 end
